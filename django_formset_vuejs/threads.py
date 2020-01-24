5946d59788c38c5c4b29ce6560406e6f34fdfa76
fatse <fatimeseelimi@gmail.com>
2020-01-24 00:25:27 -0800

    Add timer to 1h

--- django_formset_vuejs/threads.py
+++ django_formset_vuejs/threads.py
@@ -7,9 +7,9 @@ from django_formset_vuejs.models import Book
 def start_cleanup_job():
     def cleanup_db():
         while True:
+            time.sleep(60*60)
             print('hello')
             Book.objects.all().delete()
-            time.sleep(3)
 
     thread1 = threading.Thread(target=cleanup_db)
 
